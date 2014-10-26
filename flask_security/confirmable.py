83dd8c955a9cbbc9b003c116be3bf684dfc72615
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-10-26 23:17:41 +0200

    Hardwire the email confirmation link to point ot the frontend host.

--- flask_security/confirmable.py
+++ flask_security/confirmable.py
@@ -27,7 +27,8 @@ _datastore = LocalProxy(lambda: _security.datastore)
 
 def generate_confirmation_link(user):
     token = generate_confirmation_token(user)
-    return url_for_security('confirm_email', token=token, _external=True), token
+    FRONTEND_HOST = app.config.get('FRONTEND_HOST')
+    return FRONTEND_HOST + 'confirm/' + token, token
 
 
 def send_confirmation_instructions(user):
